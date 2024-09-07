9014ea82ca1e4bfd3578c2a44b10f406995a2de6
ifat <i>
2024-09-07 22:27:29 +0100

    project setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5001;
+
+//middleware
+app.use(cors());
+app.use(express());
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Simple CRUD is running!!");
+});
+
+app.listen(port, () => {
+  console.log(`Simple CRUD is running on port, ${port}`);
+});
